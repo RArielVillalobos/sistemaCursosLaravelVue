1c848f894305a57bc07a49e77ec95d727cc8b00b
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-03-31 02:04:16 -0300

    migracion,modelo y factoria de profesores,estudiantes,categorias,niveles y cursos

---
+++ app/Course.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    //
+    const PUBLISHED=1;
+    const PENDING=2;
+    //rechazado
+    const REJECTED=3;
+
+}
