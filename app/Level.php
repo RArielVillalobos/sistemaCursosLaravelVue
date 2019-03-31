a2b353bfffdd37035997b8efe79e8938690943e8
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-03-31 02:58:24 -0300

    php ide helper ayuda extra para el desarrollo en phpstorm

--- app/Level.php
+++ app/Level.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Level
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $descripcion
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level whereDescripcion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Level whereUpdatedAt($value)
+ */
 class Level extends Model
 {
     //
