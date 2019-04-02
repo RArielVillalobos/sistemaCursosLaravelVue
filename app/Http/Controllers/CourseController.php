f7b8900436d10379bbdc5b96e45996787e60b119
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-04-02 16:14:52 -0300

    obtener el detalle de un curso con sus relaciones, diferencia entre with y load

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -11,7 +11,36 @@ class CourseController extends Controller
 
     //accedemos directamente al curso
     public function show(Course $course){
+        //cuando usamos Route Model Binding(cargar modelo desde ruta no podemos usar with sino load)
+        //para que se carguen las relaciones debemos usar load y no with
+        //asi le decimos que se carguen relaciones adicionales
+        $course->load([
+            //sobrescibir al modelo, y mostrar columnas seleccionadas
+            'category'=>function($q){
+                $q->select('id','name');
+            },
+            'goals'=>function($q){
+                $q->select('id','course_id','goal');
+
+
+            },
+
+            'level'=>function($q){
+                $q->select('id','name');
+            },
+            'requirements'=>function($q){
+                $q->select('id','course_id','requirement');
+            },
+            //tambien podemos acceder a a las relaciones asi
+            //de cada reviews tambien obtener la relacion al usuario
+            'reviews.user',
+            'teacher'
+
+        ])->withCount(['students','reviews'])->get();
+
         dd($course);
+
+
 
     }
 }
